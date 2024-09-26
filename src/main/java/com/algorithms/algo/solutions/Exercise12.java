4349eba27f14dca1213d19e8e9c59aee79925e15
fallisBruno <bruno.falis@hotmail.com>
2024-09-26 09:40:23 +0100

    Exercise 12: ChatGPT solution

--- src/main/java/com/algorithms/algo/solutions/Exercise12.java
+++ src/main/java/com/algorithms/algo/solutions/Exercise12.java
@@ -7,7 +7,21 @@ import java.util.Set;
 public class Exercise12 {
 
     public static void main(String[] args) {
-        System.out.println(findSingleNumber(new int []{-1,-1,-2}));
+        System.out.println(findSingleNumberII(new int []{4,1,2,1,2}));
+    }
+
+    //ChatGPT solution using XOR, that was amazing, I had to read about XOR again like I was in university
+    public static int findSingleNumberII(int[] nums){
+        int result = 0;
+        for (int num : nums) {
+            System.out.println("result=" + Integer.toBinaryString(result));
+            System.out.println("num=" + Integer.toBinaryString(num) + "n:" + num);
+            System.out.println("operation=" + Integer.toBinaryString(result ^ num));
+            System.out.println("non binary=" + (result ^ num));
+            System.out.println("---------------------------------------------------------");
+            result ^= num;
+        }
+        return result;
     }
 
     //it solves the problem fast, but is O(N) in space complexity
