815ea96d36da35ed5c47f60a51fd43c4f60f113c
fallisBruno <bruno.falis@hotmail.com>
2024-10-13 21:11:31 +0100

    ContainsDuplicate: solution

---
+++ src/main/java/com/algorithms/algo/solutions/blind75/firstweek/ContainsDuplicate.java
@@ -0,0 +1,23 @@
+package com.algorithms.algo.solutions.blind75.firstweek;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class ContainsDuplicate {
+
+    public static void main(String[] args) {
+        System.out.println(contains(new int[]{1,2,3,4}));
+    }
+
+    public static boolean contains(int[] nums){
+        Set<Integer> controlSet = new HashSet<>();
+        for (int num : nums) {
+            if (controlSet.contains(num)) {
+                return true;
+            }
+            controlSet.add(num);
+        }
+        return false;
+    }
+
+}
