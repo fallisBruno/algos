0ed8d46ee3ecc79f86e5d3cf562f546c17481ac7
fallisBruno <bruno.falis@hotmail.com>
2024-10-15 10:54:35 +0100

    BestTimeToBuyAndSellStock: Solution

--- src/main/java/com/algorithms/algo/solutions/easy/BestTimeToBuyAndSellStock.java
+++ src/main/java/com/algorithms/algo/solutions/easy/BestTimeToBuyAndSellStock.java
@@ -3,24 +3,37 @@ package com.algorithms.algo.solutions.easy;
 public class BestTimeToBuyAndSellStock {
 
     public static void main(String[] args) {
-        System.out.println(getProfit(new int[]{2,1,2,1,0,1,2}));
+        System.out.println(getProfitII(new int[]{7,1,5,3,6,4}));
+    }
+
+    public static int getProfitII(int[] prices){
+        if(prices.length <= 1) return 0;
+        int minIndex = 0;
+        int profit = 0;
+        for (int index = 1; index < prices.length; index++) {
+            if(prices[index - 1] < prices[minIndex])
+                minIndex = index - 1;
+            if(index > minIndex && prices[index] - prices[minIndex] > profit){
+                profit = prices[index] - prices[minIndex];
+            }
+        }
+        return profit;
     }
 
     public static int getProfit(int[] prices){
         if(prices.length <= 1) return 0;
         int minIndex = 0;
         int maxIndex = 1;
-        int index = 0;
         int profit = 0;
-        while(index < prices.length){
-            if(prices[index + 1] > prices[maxIndex])
-                maxIndex = index + 1;
-            if(prices[index] < prices[minIndex])
-                minIndex = index;
-            index++;
+        for (int index = 1; index < prices.length; index++) {
+            if(prices[index] >= prices[maxIndex])
+                maxIndex = index;
+            if(prices[index - 1] <= prices[minIndex])
+                minIndex = index - 1;
             if(maxIndex > minIndex && prices[maxIndex] - prices[minIndex] > profit)
                 profit = prices[maxIndex] - prices[minIndex];
         }
+
         return profit;
     }
 
