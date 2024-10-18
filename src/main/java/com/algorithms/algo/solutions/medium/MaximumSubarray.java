034b8413222f6ea2d07be96a60cb87cb8e677efb
fallisBruno <bruno.falis@hotmail.com>
2024-10-18 10:33:40 +0100

    MaximumSubarray: First solution :)

--- src/main/java/com/algorithms/algo/solutions/medium/MaximumSubarray.java
+++ src/main/java/com/algorithms/algo/solutions/medium/MaximumSubarray.java
@@ -3,14 +3,16 @@ package com.algorithms.algo.solutions.medium;
 public class MaximumSubarray {
 
     public static void main(String[] args) {
-        System.out.println(maxSumArray(new int[]{-2,-1}));
+        System.out.println(maxSumArray(new int[]{-2,1,-3,4,-1,2,1,-5,4}));
     }
 
+    //
+    //-2,1,-3,4,-1,2,1,-5,4
     public static int maxSumArray(int[] nums){
         if(nums.length == 1) return nums[0];
-        int sum = 0, index = 0, max = Integer.MIN_VALUE;
+        int sum = nums[0], index = 1, max = nums[0];
         while(index < nums.length){
-            if(nums[index] > sum){
+            if(nums[index] > sum && sum < 0){
                 sum = nums[index];
             }else{
                 sum += nums[index];
