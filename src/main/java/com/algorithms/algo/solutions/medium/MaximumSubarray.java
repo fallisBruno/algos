2dffe66b2466ab44232692455c6759e303a0efa3
fallisBruno <bruno.falis@hotmail.com>
2024-10-18 09:40:40 +0100

    MaximumSubarray: failed nocturn attemp

--- src/main/java/com/algorithms/algo/solutions/medium/MaximumSubarray.java
+++ src/main/java/com/algorithms/algo/solutions/medium/MaximumSubarray.java
@@ -3,17 +3,17 @@ package com.algorithms.algo.solutions.medium;
 public class MaximumSubarray {
 
     public static void main(String[] args) {
-        System.out.println(maxSumArray(new int[]{-2,1,-3,4,-1,2,1,-5,4}));
+        System.out.println(maxSumArray(new int[]{-2,-1}));
     }
 
     public static int maxSumArray(int[] nums){
         if(nums.length == 1) return nums[0];
-        int sum = 0, index = 0, max = 0;
+        int sum = 0, index = 0, max = Integer.MIN_VALUE;
         while(index < nums.length){
-            if(sum + nums[index] > 0){
-                sum += nums[index];
+            if(nums[index] > sum){
+                sum = nums[index];
             }else{
-                sum = 0;
+                sum += nums[index];
             }
 
             if(sum > max) max = sum;
