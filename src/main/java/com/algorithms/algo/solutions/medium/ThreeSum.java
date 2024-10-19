ea4795b54e68d3377906921b186a1ba03c39ece6
fallisBruno <bruno.falis@hotmail.com>
2024-10-19 18:40:15 +0100

    ThreeSum: still looking for a solution

---
+++ src/main/java/com/algorithms/algo/solutions/medium/ThreeSum.java
@@ -0,0 +1,20 @@
+package com.algorithms.algo.solutions.medium;
+
+import java.util.*;
+
+public class ThreeSum {
+
+    public static void main(String[] args) {
+        System.out.println(Arrays.toString(threeSum(new int[]{-1,0,1}).toArray()));
+    }
+
+    public static List<List<Integer>> threeSum(int[] nums){
+        List<List<Integer>> result = new ArrayList<>();
+        Map<Integer, Integer> rest = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+
+        }
+        return result;
+    }
+
+}
