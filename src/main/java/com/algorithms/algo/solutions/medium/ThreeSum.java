9f11a552b1c858c9a4850495b6b1ded5d1f6e406
fallisBruno <bruno.falis@hotmail.com>
2024-10-20 12:11:47 +0100

    ThreeSum: first attempt, not working

--- src/main/java/com/algorithms/algo/solutions/medium/ThreeSum.java
+++ src/main/java/com/algorithms/algo/solutions/medium/ThreeSum.java
@@ -1,19 +1,59 @@
 package com.algorithms.algo.solutions.medium;
 
+import java.lang.reflect.Array;
 import java.util.*;
 
 public class ThreeSum {
 
     public static void main(String[] args) {
-        System.out.println(Arrays.toString(threeSum(new int[]{-1,0,1}).toArray()));
+        System.out.println(Arrays.toString(threeSum(new int[]{1,2,-2,-1}).toArray()));
     }
 
     public static List<List<Integer>> threeSum(int[] nums){
         List<List<Integer>> result = new ArrayList<>();
-        Map<Integer, Integer> rest = new HashMap<>();
-        for (int i = 0; i < nums.length; i++) {
 
+        Arrays.sort(nums);
+
+        int start = 0;
+        int end = nums.length - 1;
+        boolean hasZero = Arrays.toString(nums).contains("0");
+        Set<String> duplicateControl = new HashSet<>();
+        while(start < end){
+            //-2 = 2-(-4)
+            int sub = nums[end] + nums[start];
+            boolean walkInBoth = false;
+            System.out.println(sub + " = " + nums[end] + " - " + nums[start]);
+            List<Integer> triple = null;
+            if(sub < 0){
+                if(nums[end - 1] + sub == 0){
+                    triple = Arrays.asList(nums[end],nums[start],nums[end - 1]);
+                    walkInBoth = true;
+                }else{
+                    start++;
+                }
+            }else if(sub > 0){
+                if(nums[start + 1] + sub == 0){
+                    triple = Arrays.asList(nums[end],nums[start],nums[start + 1]);
+                    walkInBoth = true;
+                }else{
+                    end--;
+                }
+            }else if(hasZero){
+                triple = Arrays.asList(nums[end],nums[start],0);
+                walkInBoth = true;
+            }
+            if(walkInBoth || (sub == 0 && !hasZero)){
+                start++;
+                end--;
+            }
+            if(triple != null){
+                String string = Arrays.toString(triple.toArray());
+                if(duplicateControl.contains(string)) continue;
+                result.add(triple);
+                duplicateControl.add(string);
+            }
         }
+
         return result;
     }
 
