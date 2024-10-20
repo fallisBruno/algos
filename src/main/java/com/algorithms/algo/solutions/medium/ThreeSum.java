4d6fb760a8fbe4d46f2e2118f13d5b22f6e684ed
fallisBruno <bruno.falis@hotmail.com>
2024-10-20 15:41:08 +0100

    ThreeSum: Another Attempt

--- src/main/java/com/algorithms/algo/solutions/medium/ThreeSum.java
+++ src/main/java/com/algorithms/algo/solutions/medium/ThreeSum.java
@@ -1,12 +1,50 @@
 package com.algorithms.algo.solutions.medium;
 
-import java.lang.reflect.Array;
 import java.util.*;
 
 public class ThreeSum {
 
     public static void main(String[] args) {
-        System.out.println(Arrays.toString(threeSum(new int[]{1,2,-2,-1}).toArray()));
+        System.out.println(Arrays.toString(threeSumII(new int[]{-1,0,1,0}).toArray()));
+    }
+
+    public static List<List<Integer>> threeSumII(int[] nums){
+        List<List<Integer>> result = new ArrayList<>();
+        Set<String> duplicateControl = new HashSet<>();
+
+        for (int i = 0; i < nums.length; i++) {
+            int currentNumber = nums[i];
+            int left = 0, right = nums.length - 1;
+            int sum = currentNumber;
+            while (left < right){
+                if(left == i) left++;
+                if(right == i) right--;
+
+                if(nums[left] + nums[right] + currentNumber == 0){
+                    List<Integer> tripple = Arrays.asList(nums[right], nums[left], currentNumber);
+                    Collections.sort(tripple);
+                    String string = Arrays.toString(tripple.toArray());
+                    if(duplicateControl.contains(string)) break;
+                    result.add(tripple);
+                    duplicateControl.add(string);
+                }else{
+                    if(Math.abs(nums[left] + sum) <= Math.abs(sum)){
+                        sum += nums[left];
+                    }else{
+                        left++;
+                    }
+
+                    if(Math.abs(nums[right] + sum) <= Math.abs(sum)){
+                        sum += nums[right];
+                    }else{
+                        right--;
+                    }
+                }
+
+            }
+        }
+
+        return result;
     }
 
     public static List<List<Integer>> threeSum(int[] nums){
