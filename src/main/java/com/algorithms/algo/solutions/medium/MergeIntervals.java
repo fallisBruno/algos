efa28b6a49c86381a921303281d12be6d0c4d0e1
fallisBruno <bruno.falis@hotmail.com>
2024-10-21 11:05:27 +0100

    MergeIntervals: another attempt

--- src/main/java/com/algorithms/algo/solutions/medium/MergeIntervals.java
+++ src/main/java/com/algorithms/algo/solutions/medium/MergeIntervals.java
@@ -7,29 +7,44 @@ import java.util.List;
 public class MergeIntervals {
 
     public static void main(String[] args) {
-        int[][] arrays = {{1, 3}, {2, 6}, {8, 10}, {15, 18}};
-        System.out.println(Arrays.toString(merge(arrays)));
+        int[][] arrays = {{1, 4}, {0, 4}};
+        for (int[] arr : merge(arrays))
+            System.out.println(Arrays.toString(arr));
     }
 
     public static int[][] merge(int[][] intervals){
         List<List<Integer>> auxArray = new ArrayList<>();
 
         int i = 0;
-        while(i < intervals.length) {
+        int inputLen = intervals.length;
+        while(i < inputLen) {
             int first = intervals[i][0];
             int last = intervals[i][1];
-            if(intervals[i][1] >= intervals[i+1][0]){
-                while(i < intervals.length - 1 && intervals[i][1] >= intervals[i+1][0]){
-                    last = intervals[i+1][1];
+            if(i < inputLen - 1){
+                int nextFirst = intervals[i+1][0];
+                int nextLast = intervals[i+1][1];
+                if(first < nextLast && last < nextFirst){
+                    auxArray.add(Arrays.asList(first, last));
+                }else if(first > nextLast){
+                    auxArray.add(Arrays.asList(nextFirst, nextLast));
+                    auxArray.add(Arrays.asList(first, last));
+                    i++;
+                }else if(last >= nextFirst || first == nextFirst){
+                    auxArray.add(Arrays.asList(first, nextLast));
                     i++;
                 }
-            }else i++;
-            auxArray.add(Arrays.asList(first, last));
+            }
+            i++;
         }
 
-        String string = Arrays.toString(auxArray.toArray());
-        System.out.println(string);
-        return null;
+        int[][] result = new int[auxArray.size()][2];
+
+        for (int j = 0; j < auxArray.size(); j++) {
+            result[j][0] = auxArray.get(j).get(0);
+            result[j][1] = auxArray.get(j).get(1);
+        }
+
+        return result;
     }
 
 }
