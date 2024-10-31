2e22db2bfec0c073e789352f590f6421610e1c2a
fallisBruno <bruno.falis@hotmail.com>
2024-10-31 09:06:07 +0000

    MergeIntervals: solution

--- src/main/java/com/algorithms/algo/solutions/medium/MergeIntervals.java
+++ src/main/java/com/algorithms/algo/solutions/medium/MergeIntervals.java
@@ -10,6 +10,29 @@ public class MergeIntervals {
             System.out.println(Arrays.toString(arr));
     }
 
+    //I wasn't able to solve this problem, but I saw the solution and learned it :)
+    //and here it's the solution:
+    public static int[][] mergeV(int[][] intervals) {
+        Arrays.sort(intervals, Comparator.comparingInt(a -> a[0]));
+
+        List<int[]> merged = new ArrayList<>();
+        int[] prev = intervals[0];
+
+        for (int i = 1; i < intervals.length; i++) {
+            int[] interval = intervals[i];
+            if (interval[0] <= prev[1]) {
+                prev[1] = Math.max(prev[1], interval[1]);
+            } else {
+                merged.add(prev);
+                prev = interval;
+            }
+        }
+
+        merged.add(prev);
+
+        return merged.toArray(new int[merged.size()][]);
+    }
+
     public static int[][] mergeIV(int[][] intervals) {
         if (intervals.length == 1) return intervals;
 
