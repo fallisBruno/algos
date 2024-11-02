c509a845145eb11938014f492b0833c95d5ff7b8
fallisBruno <bruno.falis@hotmail.com>
2024-11-02 09:19:33 +0000

    MaximumProductSubarray: attempts

--- src/main/java/com/algorithms/algo/solutions/medium/MaximumProductSubarray.java
+++ src/main/java/com/algorithms/algo/solutions/medium/MaximumProductSubarray.java
@@ -3,7 +3,7 @@ package com.algorithms.algo.solutions.medium;
 public class MaximumProductSubarray {
 
     public static void main(String[] args) {
-        System.out.println(maxProduct(new int[]{-2,0,-1}));
+        System.out.println(maxProduct(new int[]{0,2}));
     }
 
     public static int maxProduct(int[] nums){
@@ -12,14 +12,12 @@ public class MaximumProductSubarray {
         int largestProduct = Integer.MIN_VALUE, max = nums[0];
 
         for (int i = 0; i < nums.length - 1; i++) {
-            while(max > largestProduct){
-                largestProduct = max;
-                max *= nums[i+1];
-            }
-            if()
+            if( max > largestProduct) largestProduct = max;
+            if(max == 0) max = 1;
+            max *= nums[i+1];
         }
 
-        return largestProduct;
+        return Math.max(max, largestProduct);
     }
 
 }
