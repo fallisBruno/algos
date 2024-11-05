4e15081bfbe6ae07635e6bbbae3a6bd00f867fbd
fallisBruno <bruno.falis@hotmail.com>
2024-11-05 10:24:30 +0000

    MaximumProductSubarray: another attempt, 153/190 testcases passed

--- src/main/java/com/algorithms/algo/solutions/medium/MaximumProductSubarray.java
+++ src/main/java/com/algorithms/algo/solutions/medium/MaximumProductSubarray.java
@@ -3,21 +3,44 @@ package com.algorithms.algo.solutions.medium;
 public class MaximumProductSubarray {
 
     public static void main(String[] args) {
-        System.out.println(maxProduct(new int[]{0,2}));
+        System.out.println(maxProduct(new int[]{2,3,-2,4}));
+        System.out.println(maxProduct(new int[]{7,-2,-4}));
+        System.out.println(maxProduct(new int[]{7,-2,-4}));
     }
 
     public static int maxProduct(int[] nums){
         if(nums.length == 1) return nums[0];
 
-        int largestProduct = Integer.MIN_VALUE, max = nums[0];
+        int largestProduct = Integer.MIN_VALUE;
+        int max = 1;
+        int index = 0;
 
-        for (int i = 0; i < nums.length - 1; i++) {
-            if( max > largestProduct) largestProduct = max;
-            if(max == 0) max = 1;
-            max *= nums[i+1];
+        while(index < nums.length){
+            if(nums[index] <= 0){
+                int firstNegative = index;
+                int maxAux = 1;
+                ++index;
+                while(index < nums.length && nums[index] > 0){
+                    maxAux *= nums[index];
+                    largestProduct = Math.max(maxAux, largestProduct);
+                    index++;
+                }
+                if(index >= nums.length) return largestProduct;
+                if(nums[index] < 0){
+                    max *= nums[index] * nums[firstNegative] * maxAux;
+                    largestProduct = Math.max(max, largestProduct);
+                }else{
+                    largestProduct = Math.max(0, largestProduct);
+                    max = 1;
+                }
+            }else{
+                max *= nums[index];
+                largestProduct = Math.max(max, largestProduct);
+            }
+            index++;
         }
 
-        return Math.max(max, largestProduct);
+        return largestProduct;
     }
 
 }
