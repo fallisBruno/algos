b4974f3f4327b6a254a4a8dfe13f8c911d65c127
fallisBruno <bruno.falis@hotmail.com>
2024-11-06 11:26:35 +0000

    MaximumProductSubarray: new failed attempt

--- src/main/java/com/algorithms/algo/solutions/medium/MaximumProductSubarray.java
+++ src/main/java/com/algorithms/algo/solutions/medium/MaximumProductSubarray.java
@@ -3,9 +3,42 @@ package com.algorithms.algo.solutions.medium;
 public class MaximumProductSubarray {
 
     public static void main(String[] args) {
-        System.out.println(maxProduct(new int[]{2,3,-2,4}));
+/*        System.out.println(maxProduct(new int[]{2,3,-2,4}));
         System.out.println(maxProduct(new int[]{7,-2,-4}));
-        System.out.println(maxProduct(new int[]{7,-2,-4}));
+        System.out.println(maxProduct(new int[]{2,-5,-2,-4,3}));
+        System.out.println(maxProduct(new int[]{-2,0,-1}));*/
+
+
+        System.out.println(maxProductI(new int[]{-2,10,-1}));
+    }
+
+    public static int maxProductI(int[] nums){
+        if(nums.length == 1) return nums[0];
+
+        int product = Integer.MIN_VALUE, cumulative = 1;
+
+        for (int i = 0; i < nums.length; i++) {
+            cumulative *= nums[i];
+            product = Math.max(cumulative, product);
+            if(cumulative == 0) cumulative = 1;
+            else if(cumulative < 0){
+                int cumulativeAux = 1;
+                int indexAux = i + 1;
+                while(indexAux < nums.length && nums[indexAux] > 0){
+                    cumulativeAux *= nums[indexAux];
+                    indexAux++;
+                    product = Math.max(cumulativeAux, product);
+                }
+                if(indexAux >= nums.length) return product;
+                if(nums[indexAux] == 0) cumulative = 1;
+                else {
+                    cumulative *= nums[i] * nums[indexAux] * cumulativeAux;
+                    product = Math.max(cumulative, product);
+                }
+            }
+        }
+
+        return product;
     }
 
     public static int maxProduct(int[] nums){
